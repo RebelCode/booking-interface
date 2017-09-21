1fdc0f6fbdda1d700a78c0903be5db4c8121504e
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-21 10:51:11 +0200

    Added test for `BookingInterface`

---
+++ test/unit/BookingInterfaceTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace RebelCode\Bookings\UnitTest;
+
+use Xpmock\TestCase;
+use RebelCode\Bookings\BookingInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class BookingInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\Bookings\BookingInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->getStart()
+            ->getEnd()
+            ->getStatus()
+            ->transition();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+
+        $this->assertInstanceOf(
+            'RebelCode\Bookings\PeriodInterface',
+            $subject,
+            'Test subject does not extend parent interface.'
+        );
+
+        $this->assertInstanceOf(
+            'RebelCode\Bookings\StatusAwareInterface',
+            $subject,
+            'Test subject does not extend parent interface.'
+        );
+
+        $this->assertInstanceOf(
+            'RebelCode\Bookings\TransitionCapableInterface',
+            $subject,
+            'Test subject does not extend parent interface.'
+        );
+    }
+}
