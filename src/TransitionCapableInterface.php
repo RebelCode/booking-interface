accb2eb976e4afda34c6de71abb9d08ec1732f93
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-22 11:18:31 +0200

    Transitioning no longer requires the data param
    The transition param can be a stringable object.
    Consumer code can type-check this parameter if it needs to obtain additional data.

--- src/TransitionCapableInterface.php
+++ src/TransitionCapableInterface.php
@@ -3,7 +3,6 @@
 namespace RebelCode\Bookings;
 
 use Dhii\Util\String\StringableInterface as Stringable;
-use Psr\Container\ContainerInterface as Container;
 use RebelCode\Bookings\Exception\BookingExceptionInterface;
 use RebelCode\Bookings\Exception\CouldNotTransitionExceptionInterface;
 
@@ -20,12 +19,11 @@ interface TransitionCapableInterface
      * @since [*next-version*]
      *
      * @param string|Stringable $transition The transition code.
-     * @param array|Container   $data       Optional data to use for transitioning.
      *
      * @throws CouldNotTransitionExceptionInterface If the transition failed or was aborted.
      * @throws BookingExceptionInterface            If an error was encountered during transition.
      *
      * @return TransitionCapableInterface The instance with the completed transition - may not be the same instance.
      */
-    public function transition($transition, $data = array());
+    public function transition($transition);
 }
