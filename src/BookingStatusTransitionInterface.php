612cb19a6174007015b92709935637a85ab353e3
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-26 12:45:22 +0200

    Updated PHP doc description ...
    ... for `BookingStatusTransitionInterface`.

--- src/BookingStatusTransitionInterface.php
+++ src/BookingStatusTransitionInterface.php
@@ -8,18 +8,19 @@ use Psr\Container\ContainerInterface;
 /**
  * Something that represents a transition for a booking status.
  *
- * Objects that implement this interface can be used as argument for {@link BookingInterface::transition()}.
+ * Objects that implement this interface can be used as an argument for {@link BookingInterface::transition()}.
  * Therefore, the {@link __toString()} method SHOULD return a valid transition key. Any other arbitrary data can be
- * attached to the transition and can be later retrieved via {@link ContainerInterface::get()}.
+ * attached to the transition instance, which can be later retrieved via {@link ContainerInterface::get()}.
  *
  * The {@link getBooking()} method allows external components to obtain the booking undergoing transition directly
- * from the transition instance.
+ * from the transition instance. Thus, components can depend on consuming a transition instance, rather than having to
+ * fetch a booking instance from some other component.
  *
  * It is recommended to construct instances that implement this interface only at the required stage. For instance,
  * a booking can internally construct a transition object when its {@link transition()} method is invoked, and then
  * pass such an instance to other components. This is to ensure interoperability between bookings and other state
- * machines by having the transition be initially a string, that may be turned into an object (possible with a different
- * transition key) if required.
+ * machines by having the transition be initially a string, that may be turned into an object if required (possibly
+ * with a different transition key).
  *
  * @since [*next-version*]
  */
