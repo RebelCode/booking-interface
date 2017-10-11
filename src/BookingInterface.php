668bbb725f66192aa500fc97c3c4de29c165225b
Miguel Muscat <miguelmuscat93@gmail.com>
2017-10-11 16:34:54 +0200

    Reworded description for `BookingInterface`

--- src/BookingInterface.php
+++ src/BookingInterface.php
@@ -8,11 +8,13 @@ use Dhii\Time\PeriodInterface;
 /**
  * Something that represents a booking.
  *
- * A booking is a period of time that is reserves something for that period of time.
+ * A booking is a period of time during which something is reserved, like a service or resource.
  *
  * This interface provides the foundation for all booking-related logic, by making the booking aware of a status.
- * While being a very simple concept, the status signifies the current state of the booking and can be used to
- * signal changes made to the booking.
+ * While being very simple in concept, the status signifies the current state of the booking.
+ *
+ * Through this awareness, other components in a system can take action on booking status changes as well as influence
+ * the booking, and hence possibly other components, through status changes.
  *
  * @since [*next-version*]
  */
