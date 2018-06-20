cada590ed6fc0c3b5f59296158550dfde0f2961a
Miguel Muscat <miguelmuscat93@gmail.com>
2018-06-20 10:53:39 +0200

    Added data key for factory config

--- src/BookingFactoryInterface.php
+++ src/BookingFactoryInterface.php
@@ -12,32 +12,39 @@ use Dhii\Factory\FactoryInterface;
 interface BookingFactoryInterface extends FactoryInterface
 {
     /**
+     * The key for the booking data in the factory config.
+     *
+     * @since [*next-version*]
+     */
+    const K_DATA = 'data';
+
+    /**
      * The key for the ID in the factory config.
      *
      * @since [*next-version*]
      */
-    const K_ID = 'id';
+    const K_DATA_ID = 'id';
 
     /**
      * The key for the start in the factory config.
      *
      * @since [*next-version*]
      */
-    const K_START = 'start';
+    const K_DATA_START = 'start';
 
     /**
      * The key for the end in the factory config.
      *
      * @since [*next-version*]
      */
-    const K_END = 'end';
+    const K_DATA_END = 'end';
 
     /**
      * The key for the status in the factory config.
      *
      * @since [*next-version*]
      */
-    const K_STATUS = 'status';
+    const K_DATA_STATUS = 'status';
 
     /**
      * {@inheritdoc}
